3a2d7bed32edc497997c12f3ad5d2ef0c8a32d04
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2024-01-25 09:19:59 -0300

    Initial commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+    public static void main (String[] args){
+        String saludar = "Hola Mundo";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+        
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        if (num)
+    }
+}
+
