abb86de474a7d5d856c7beced333adbcd2042638
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2024-01-25 21:44:20 -0300

    updates

--- src/SistemasNumericos.java
+++ src/SistemasNumericos.java
@@ -1,21 +1,41 @@
+import javax.swing.*;
+import java.util.jar.JarOutputStream;
+
 public class SistemasNumericos {
 
     public static void main(String[] args) {
+
+        String numeroString = (JOptionPane.showInputDialog(null,"Ingrese un número entero"));
+        int numeroDecimal = 0;
+
+        try {
+            numeroDecimal = Integer.parseInt(numeroString);
+
+        } catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null , "Error, ingrese un número entero");
+            main(args);
+            return;
+
+        }
+
+        String resultadoBinario = "numeroBinario = " + numeroDecimal + " = "+ Integer.toBinaryString((numeroDecimal));
         
-        int numeroDecimal = 500;
-        System.out.println("numeroDecimal = " + numeroDecimal);
-        System.out.println("numeroDecimal = " + numeroDecimal + " = "+ Integer.toBinaryString((numeroDecimal)));
-        
-        int numeroBinario = 0b111110100;//agregar 0b para transformar a binario
-        System.out.println("numeroBinario = " + numeroBinario);
+        //int numeroBinario = 0b111110100;//agregar 0b para transformar a binario
+        //System.out.println("numeroBinario = " + numeroBinario);
 
+        String resultadoOctal = "numeroOctal = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
 
-        System.out.println("numeroOctal = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal) );
-        int numeroOctal = 0764;
-        System.out.println("numeroOctal = " + numeroOctal);
+        //int numeroOctal = 0764;
+        //System.out.println("numeroOctal = " + numeroOctal);
 
-        System.out.println("numeroHexadecimal = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
-        int numeroHexadecimal = 0x1f4;
-        System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+        String resultadoHexa = "numeroHexadecimal = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        /*int numeroHexadecimal = 0x1f4;
+        System.out.println("numeroHexadecimal = " + numeroHexadecimal);*/
+
+        String mensaje = resultadoBinario;
+        mensaje += "\n" +  resultadoOctal;
+        mensaje += "\n" + resultadoHexa;
+
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 }
