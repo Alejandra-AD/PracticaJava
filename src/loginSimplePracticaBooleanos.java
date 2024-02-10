4c3e4797b6466d351772ba322a50aa5f10c4b7e0
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2024-02-10 19:31:50 -0300

    updates

---
+++ src/loginSimplePracticaBooleanos.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class loginSimplePracticaBooleanos {
+    public static void main(String[] args) {
+
+        String user = "Alejandra", pass = "martini123";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el usuario: ");
+        
+        String usuario = scanner.nextLine();
+        System.out.println("usuario = " + usuario);
+
+        System.out.println("Ingrese contraseña: ");
+        String contrasena = scanner.nextLine();
+        System.out.println("contraseña = " + contrasena);
+
+        if ((user.equals(usuario)) && (pass.equals(contrasena))){
+            System.out.println("Hola! " + usuario);
+        } else{
+            System.out.println("usuario o contraseña incorrectos");
+        }
+
+
+        
+        
+    }
+}
