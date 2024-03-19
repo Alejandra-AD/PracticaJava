170e86cfdf8646b45e758db1c28b98c0f0064ac3
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-03-19 14:39:17 -0300

    updates

---
+++ src/OperadorTernarioNumeroMayor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OperadorTernarioNumeroMayor {
+    public static void main(String[] args) {
+
+        Scanner scanNumero = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número: ");
+        int num1 = scanNumero.nextInt();
+
+        System.out.println("Ingrese el segundo número: ");
+        int num2 = scanNumero.nextInt();
+
+        System.out.println("Ingrese el tercer número");
+        int num3 = scanNumero.nextInt();
+        
+        int numMayor = (num1 > num2)? num1:num2;
+        numMayor = (numMayor>num3)? numMayor:num3;
+
+        System.out.println("numMayor = " + numMayor);
+
+    }
+}
