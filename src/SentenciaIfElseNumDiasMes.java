f6c409ea5f05690a4ca75a5163c681c57ccb458f
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-03-22 16:30:07 -0300

    updates

---
+++ src/SentenciaIfElseNumDiasMes.java
@@ -0,0 +1,22 @@
+public class SentenciaIfElseNumDiasMes {
+    public static void main(String[] args) {
+        int year = 1800;
+
+        //función años bisiesto
+
+/*        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                System.out.println(year + " es bisiesto");
+            } else if (year % 100 == 0 && year % 400 == 0) {
+                System.out.println(year + " es bisiesto");
+            }else{
+                System.out.println("no es bisiesto");
+            }
+        }else {
+            System.out.println("no es bisiesto");
+        }*/
+
+
+
+    }
+}
