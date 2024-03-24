9b9bbd1f02128e40bb3939c3623fc903e20e88fe
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-03-23 21:45:44 -0300

    updates

---
+++ src/SentenciaForEach.java
@@ -0,0 +1,11 @@
+public class SentenciaForEach {
+    public static void main(String[] args) {
+        
+        int [] numeros = {1,2,3,4,5,6,7,8}; 
+        
+        for( int num: numeros){//for each
+            System.out.println("num = " + num);
+        }
+        
+    }
+}
