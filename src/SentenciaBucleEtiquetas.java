c0973484b9a2974348bfd31805c0349ec5b7c732
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-03-24 11:57:43 -0300

    updates

---
+++ src/SentenciaBucleEtiquetas.java
@@ -0,0 +1,32 @@
+public class SentenciaBucleEtiquetas {
+    public static void main(String[] args) {
+        
+        etiquetaBucle1:
+        for (int i=0 ; i < 5 ;i++){
+            System.out.println("i = " + i);
+            for (int j = 0 ; j < 5 ;j++ ){
+                if(i==2){
+                    continue etiquetaBucle1;
+                }
+                System.out.println("i="+ i + " y "+ "j=" + j);
+            }
+        }
+
+        etiquetaBucle2:
+        for (int i = 0; i < 5 ; i++){
+            System.out.println("i = " + i);
+            int j = 0;
+            while (j < 5){
+                System.out.println("i="+ i + " y "+ "j=" + j);
+                if(i == 3){
+                    break etiquetaBucle2;
+                }
+                j++;
+            }
+
+
+        }
+
+        
+    }
+}
