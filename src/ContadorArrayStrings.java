96a295e37a2410862ecca78947586d34a9487db9
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-03-26 22:16:13 -0300

    updates

---
+++ src/ContadorArrayStrings.java
@@ -0,0 +1,14 @@
+public class ContadorArrayStrings {
+    public static void main(String[] args) {
+
+        String [] dankyNoga = {"DankyNogatongamegalosomanjarchafafrinilofo"};
+        
+        int nroElementosArray = dankyNoga.length;
+        System.out.println("nroElementosArray = " + nroElementosArray);
+        
+        int nroCaracteresString = dankyNoga[0].length();
+        System.out.println("nroCaracteresString = " + nroCaracteresString);
+
+        
+    }
+}
